c553e3a598cb7c5737e65b633739255cd7fb5acd
Bonny94 <bonazzigiacomo@yahoo.it>
2026-02-21 02:33:57 +0100

    fix: chang config

--- prj_oroscopo/config.py
+++ prj_oroscopo/config.py
@@ -1,6 +1,6 @@
 import os
-from azure.identity import ClientSecretCredential
-from azure.keyvault.secrets import SecretClient 
+from azure.identity import DefaultAzureCredential
+from azure.keyvault.secrets import SecretClient
 
 SMTP_SERVER = 'smtp.gmail.com'
 SMTP_PORT = 587
@@ -8,10 +8,9 @@ FROM_EMAIL = 'oroscopobonny@gmail.com'
 FROM_PASSWORD = 'xkza upxn cbng vubq'
 
 def open_ai_credentials():
-    tenantId = os.getenv("TENANT_ID")
-    clientId = os.getenv("CLIENT_ID")
-    clientSecret = os.getenv("CLIENT_SECRET")
-    credential = ClientSecretCredential(tenantId, clientId, clientSecret)
-    client = SecretClient(vault_url = 'https://funkeyvault.vault.azure.net/', credential = credential)
-
+    credential = DefaultAzureCredential()
+    client = SecretClient(
+        vault_url='https://funkeyvault.vault.azure.net/',
+        credential=credential,
+    )
     return client.get_secret("OpenAI").value
